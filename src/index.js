160acd7a6620d0145cfe609ae651731bb2158198
Amy Young <youngamy1223@gmail.com>
2022-01-07 22:53:34 -0700

    separate service and UI logic, create weather-service file

--- src/index.js
+++ src/index.js
@@ -2,41 +2,33 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './weather-service.js'
+
+function clearFields() {
+  $('#city').val("");
+  $('#state').val("");
+  $('.showErrors').text("");
+  $('.showHumidity').text("");
+  $('.showTemp').text("");
+  $('.showWindSpeed').text("");
+}
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     const city = $('#city').val();
     const state = $('#state').val();
-    $('#location').val("");
+    clearFields();
 
-    let promise = new Promise(function(resolve, reject) {
+    let promise = WeatherService.getWeather(city);
+    promise.then(function(response) {
+        const body = JSON.parse(response);
+          $('.showHumidity').text(`The humidity in ${city}${state} is ${body.main.humidity}%`);
+          $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+          $('.showWindSpeed').text(`The wind speed is ${body.wind.speed} m/s.`);
+          $('.showErrors').text("");
+      }, function(error) {
+        $('.showErrors').text(`There was an error processing your request: ${error}`);
+    })
 
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=${process.env.API_KEY}`;
-
-    request.onload = function() {
-      if (this.status === 200) {
-        resolve(request.response);
-      } else {
-        reject(request.response);
-      }
-    }
-
-    request.open("GET", url, true);
-    request.send();
-  });
-
-  promise.then(function(response) {
-    const body = JSON.parse(response);
-      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-      $('.showWindSpeed').text(`The wind speed is ${body.wind.speed} m/s.`);
-      $('.showErrors').text("");
-  }, function(error) {
-    $('.showErrors').text(`There was an error processing your request: ${error}`);
-      $('.showHumidity').text("");
-      $('.showTemp').text("");
-      $('.showWindSpeed').text("");
     });
   });
-});
